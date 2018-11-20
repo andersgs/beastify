805a0f8cd27eb6677ca1623fdd0c40b0e03231bd
Anders Goncalves da Silva <andersgs@gmail.com>
2018-11-19 16:46:13 -0800

    Clean up comments

--- beastify/__main__.py
+++ beastify/__main__.py
@@ -25,37 +25,12 @@ def run_tests(ctx, param, value):
 
 
 @click.command()
-# @click.option("--path", \
-#                 help = "Path to SNIPPY output folder.",
-#                 default = None)
-# @click.option("--nb", \
-#                 help = "Select N random genes from list", \
-#                 default = None,
-#                 type = int)
 @click.option("--out",
               help="Outfile name (default: out.nexus)",
               default='out.nexus')
-# @click.option("--snippy", \
-#                 help="Use snippy-core, and filter with 'random' or 'top' or 'all' (default: all). If specified 'random' or 'top', then --n must be specified too.", \
-#                 default = "all")
-# @click.option("--feature", \
-#                 help="Feature name to search in Genbank file (default: CDS)", \
-#                 default = "CDS")
 @click.option("--info",
               help="Path to a tab-delimited file with two or more columns. The first column has the isolate ID, and other columns have dates, location, etc. The information will be added to the isolate ID in the same order as the columns",
               default=None)
-# @click.option("--core_filename", \
-#                 help = "If using --snippy, name of filenam containing SNP data (default: core.tab)",\
-#                 default = "core.tab")
-# @click.option("--seq_filename", \
-#                 help = "The name of the snippy alignment file to search for (default: snps.consensus.subs.fa)", \
-#                 default = "snps.consensus.subs.fa")
-# @click.option("--gene_list", \
-#                 help = "A list of genes to include in the nexus file.", \
-#                 default = None)
-# @click.option("--exclude", \
-#                 help = "Comma separated list of isolates to exclude (default: None). Example: Iso22,Iso34", \
-#                 default = "")
 @click.option("--inc_ref",
               help="Whether to include the reference in the final out file (default: False)",
               is_flag=True,
@@ -85,11 +60,6 @@ def beastify(reference, out, info, inc_ref, aln_file, aln_file_format, subsample
 
     By Anders Goncalves da Silva
     '''
-    # transforming exclude list to a Python list
-    # if exclude != None:
-    #     exclude = exclude.split(",")
-    # start by parsing genbank file, and loading features
-    #import pdb; pdb.set_trace()
     if parts == '1,2,3,4,5':
         parts = [1, 2, 3, 4, 5]
     else:
@@ -105,34 +75,6 @@ def beastify(reference, out, info, inc_ref, aln_file, aln_file_format, subsample
         collection.load_alignment(aln_file, aln_file_format)
     else:
         raise ValueError("beastify only accepts alignments for the moment.")
-    # elif snippy != None and nb != None:
-    #     genes.parse_snippycore(path = path, \
-    #         corefn = core_filename, \
-    #         nb = nb, \
-    #         sample = snippy)
-    #     genes.load_features( \
-    #                         gene_list = None, \
-    #                         nb = nb, \
-    #                         feature = feature)
-    # elif snippy.lower() in ['random', 'top'] and nb == None:
-    #     raise ValueError("If specifying --snippy, then --nb must be specified too.")
-    # elif snippy.lower() == 'all':
-    #     genes.parse_snippycore(path = path, \
-    #                 corefn = core_filename, \
-    #                 nb = nb, \
-    #                 sample = 'all')
-    #     genes.load_features( \
-    #                         gene_list = None, \
-    #                         nb = None, \
-    #                         feature = feature)
-    # else:
-    #     genes.load_features( \
-    #                         gene_list = gene_list, \
-    #                         nb = nb, \
-    #                         feature = feature)
-    # collection.load_isolates(path = path, \
-    #                         seq_file = seq_filename, \
-    #                         ignore = exclude)
     if info != None:
         print(('#'*80))
         collection.add_info(info)
